e8396939d4a209b3117de00487120ab441a1239d
Nick Heiner <NickHeiner@users.noreply.github.com>
2025-06-21 18:40:33 +0000

    Add devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,6 @@
+echo 'alias cc="npx -g @anthropic-ai/claude-code"' >> ~/.bashrc
+echo 'alias gst="git status"' >> ~/.bashrc
+echo 'alias gp="git push"' >> ~/.bashrc
+echo 'alias gd="git diff"' >> ~/.bashrc
+echo 'alias gcam="git commit -am"' >> ~/.bashrc
+npm install -g @anthropic-ai/claude-code
